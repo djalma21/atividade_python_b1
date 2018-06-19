934d4789c1da5ae4188af2c52bab0efbf87f2fee
djalma21 <40030393+djalma21@users.noreply.github.com>
2018-06-19 09:19:16 -0300

    Update Questao4.py

--- Questao4.py
+++ Questao4.py
@@ -2,12 +2,15 @@ l1=int(input("informe o lado do trinagulo: "))
 l2=int(input("informe o lado do trinagulo: "))
 l3=int(input("informe o lado do trinagulo: "))
 
-if l1+l2<l3:
+if l1+l2<l3 or l1+l3< l2 or l2+l3< l1:
     print("essas medidas não forma um triangulo")
     exit()
+
 if l1==l2 and l2==l3:
     print("é um triangulo equilatero!")
+
 elif l1==l2 or l1==l2 or l2==l3:
     print("é um triangulo isóceles!")
+
 else:
     print("é um triangulo escaleno")
